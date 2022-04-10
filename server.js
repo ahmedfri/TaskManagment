32ebeeb3503034c1c178f374cf24aae51187e466
ahmed.fri <ahmed.friend19@gmail.com>
2022-04-11 01:26:17 +0400

    ccz

--- server.js
+++ server.js
@@ -1,18 +1,8 @@
-//Install express server
 const express = require('express');
 const path = require('path');
-
 const app = express();
-
-// Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/TASKMANAGMENT'));
-
 app.get('/*', function(req,res) {
-    
-// res.sendFile(path.join('./dist/TASKMANAGMENT/login.html'));
-// res.sendFile('index.html', { root: __dirname });
-res.sendFile(path.join(__dirname,  'index.html'));
-});
-
-// Start the app by listening on the default Heroku port
+res.sendFile(path.join(__dirname+
+'/dist/TASKMANAGMENT/index.html'));});
 app.listen(process.env.PORT || 8080);

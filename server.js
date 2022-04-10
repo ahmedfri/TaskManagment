d619f5f875a9e5f0655db3fed17a57397e512a2d
ahmed.fri <ahmed.friend19@gmail.com>
2022-04-11 00:55:41 +0400

    ccxcz

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ app.use(express.static(__dirname + '/dist/TASKMANAGMENT'));
 app.get('/*', function(req,res) {
     
 // res.sendFile(path.join('./dist/TASKMANAGMENT/login.html'));
-res.sendFile('index.html', { root: __dirname });
+// res.sendFile('index.html', { root: __dirname });
+res.sendFile(path.join(__dirname, 'app', 'index.html'));
 });
 
 // Start the app by listening on the default Heroku port

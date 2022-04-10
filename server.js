175190ea176eed03cd4a987f6f0e15a40ff7ff5d
ahmed.fri <ahmed.friend19@gmail.com>
2022-04-11 01:02:20 +0400

    cc

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get('/*', function(req,res) {
     
 // res.sendFile(path.join('./dist/TASKMANAGMENT/login.html'));
 // res.sendFile('index.html', { root: __dirname });
-res.sendFile(path.join(__dirname, 'app', 'index.html'));
+res.sendFile(path.join(__dirname,  'index.html'));
 });
 
 // Start the app by listening on the default Heroku port
